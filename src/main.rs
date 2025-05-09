4b3d1cfd9a0baafcb16f341613315b926842aa5e
Karol Moroz <karol@moroz.dev>
2025-05-09 23:31:55 +0800

    Add number of attempts and elapsed time

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
+use std::time::Instant;
 
 fn main() {
     println!("Guess the number!");
 
+    let mut count = 1;
     let secret_number = rand::rng().random_range(1..=100);
 
+    let start_time = Instant::now();
     loop {
         println!("Please input your guess.");
         let mut guess = String::new();
@@ -20,13 +23,17 @@ fn main() {
             Err(_) => continue,
         };
 
+        let elapsed = start_time.elapsed().as_secs_f64();
+
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
-            }
+            Ordering::Less => println!("Too small! Time elapsed: {elapsed:.2}s"),
+            Ordering::Greater => println!("Too big! Time elapsed: {elapsed:.2}s"),
+            Ordering::Equal => break,
         }
+
+        count += 1;
     }
+
+    let elapsed = start_time.elapsed().as_secs_f64();
+    println!("You win in {count} attempts. Time elapsed: {elapsed:.2}s");
 }
